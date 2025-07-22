f015a65355f475a0af6003cf1eb78ad7f9a7817c
Siddharth-1913 <98050209+siddharth1913@users.noreply.github.com>
2025-07-23 00:18:18 +0530

    added code to check even or odd with 2 approaches

--- Logic_Building/CheckEvenOrOdd.cpp
+++ Logic_Building/CheckEvenOrOdd.cpp
@@ -1,1 +1,18 @@
-git 
+/*Approach - 1 :  */
+
+#include <iostream>
+using namespace std;
+
+int main (){
+    int n;
+    cout<<"Take input : ";
+    cin >> n;
+
+    if(n%2 == 0)
+    {
+        cout<<"Number is even"<<endl;
+    }
+    else{
+        cout<<"Number is odd"<<endl;
+    }
+}
