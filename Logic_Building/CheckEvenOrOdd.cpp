5431560fe9fa50c24ecc303128181f5ab0d8b59b
Siddharth-1913 <98050209+siddharth1913@users.noreply.github.com>
2025-07-23 00:19:27 +0530

    added 2 approach to check for odd & even

--- Logic_Building/CheckEvenOrOdd.cpp
+++ Logic_Building/CheckEvenOrOdd.cpp
@@ -1,18 +1,47 @@
-/*Approach - 1 :  */
-
 #include <iostream>
 using namespace std;
 
-int main (){
-    int n;
-    cout<<"Take input : ";
-    cin >> n;
+/* [Naive Approach] By Finding the Remainder - O(1) Time and O(1) Space */
+void findByReminder(int n){
 
-    if(n%2 == 0)
-    {
+    if(n%2 == 0){
+        cout<<"Number is even"<<endl;
+    }else{
+        cout<<"Number is odd"<<endl;
+    }   
+} 
+
+/* [Efficient Approach] Using Bitwise AND Operator - O(1) Time and O(1) Space */
+/* The last bit of all odd numbers is always 1, while for even numbers it’s 0. 
+So, when performing bitwise AND operation with 1, odd numbers give 1, 
+and even numbers give 0. */
+bool usingBitwise_AND_Operator(int n){  
+    if( n & 1){
+        cout<<"Number is odd"<<endl;
+    }else{
         cout<<"Number is even"<<endl;
     }
-    else{
-        cout<<"Number is odd"<<endl;
+}
+
+
+int main (){
+    int num, option;
+    cout<<"Take input : ";
+    cin >> num;
+
+    cout<<"Choose method to perfom : ";
+    cin>> option;
+
+    switch(option){
+        case 1: 
+            findByReminder(num);
+            break;
+        case 2: 
+            usingBitwise_AND_Operator(num);
+            break;
+        deafult:
+            cout<<"Invalid Option"<<endl;
+            break;
     }
+    
 }
